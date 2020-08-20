62de27c1d49d3b133bdc5fbbcb2dd4add4923d53
Leo Nesfield <leo@thelmgn.com>
2020-08-20 12:34:08 +0100

    site updates

--- client.js
+++ client.js
@@ -31,21 +31,47 @@ window.onkeydown = function(evt) {
     if ((evt.code == "Minus" || evt.code == "Equal") && (evt.ctrlKey || evt.metaKey)) {evt.preventDefault()}
 }
 
-
+getLatest = ((j,platform) => {
+    for (var r of j) {
+        for (var asset of r.assets) {
+            if (asset.name.includes(platform)) return {asset,r};
+        }
+    }
+    return false;
+})
+document.querySelector("#loginContainer").onclick = () => { document.body.classList.remove("loggingIn") }
 async function download() {
     /**if (confirm("IMPORTANT: READ CAREFULLY\n\nJellyfish is only to be used on games that you have explicit permission to run a LSI on, such as for example, developing anti-cheat software.\n\nAre you intending to use Jellyfish to attack into games you are not the owner of, and do not have permission from the owner to run a LSI on?\n\nCancel = No\nOK = Yes")) {
         localStorage.setItem("user_is_skid",true)
         location.reload()
     }**/
-    injectBtn.innerText = "Fetching download URL"
-    injectBtn.disabled = true
+    document.body.classList.add("loggingIn")
     var ftch = await fetch("https://api.github.com/repositories/273986462/releases")
     var j = await ftch.json()
+    var latestMac = getLatest(j,"darwin")
+    if (latestMac) {
+        document.querySelector("#macBtn").disabled = undefined
+        document.querySelector("#macBtn").innerText = "Download " + latestMac.r.tag_name + " for macOS"
+        document.querySelector("#macBtn").onclick = () => {location.replace(latestMac.asset["browser_download_url"])}
+    } else {
+        document.querySelector("#macBtn").innerText = "macOS version coming soon"
+    }
+    var latestWin = getLatest(j,"win32")
+    console.log(latestMac,latestWin)
+    if (latestWin) {
+        document.querySelector("#winBtn").disabled = undefined
+        document.querySelector("#winBtn").innerText = "Download " + latestWin.r.tagName + " for Windows"
+        document.querySelector("#winBtn").onclick = () => {location.replace(latestWin.asset["browser_download_url"])}
+    } else {
+        document.querySelector("#winBtn").innerText = "Windows version coming soon"
+    }
+    /**
     injectBtn.innerText = "Starting download"
     location.replace(j[0].assets[0].browser_download_url)
     injectBtn.innerText = "Downloading"
     setTimeout(function() {
         injectBtn.innerText = "Download"
         injectBtn.disabled = false
-    },2000)
+    },2000)**/
+    
 }
